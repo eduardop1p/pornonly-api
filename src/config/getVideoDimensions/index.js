f50ae73dd2ee79dcd5033d56840e7a52adb0f950
eduardop1p <el702985@gmail.com>
2023-10-23 19:33:44 -0300

    adcionando suporte a duração de video

--- src/config/getVideoDimensions/index.js
+++ src/config/getVideoDimensions/index.js
@@ -5,23 +5,40 @@ const axios = require('axios');
 
 const promisifiedExec = util.promisify(cp.exec);
 
+function videoDurationHydration(duration) {
+  // eslint-disable-next-line
+  const hours = Math.floor(duration / 3600);
+  const minutes = Math.floor((duration % 3600) / 60);
+  const segunds = Math.floor(duration % 60);
+  const zeroLeft = value => (value > 9 ? value : `0${value}`);
+
+  return `${minutes}:${zeroLeft(segunds)}`;
+}
+
 module.exports = async function getVideoDimensions(videoUrl) {
   // atenção! este comando não irar funcionar se o caminho do terminal estiver com caracteris não aceitos, por exemplo espaçamentos
-  const command = `${ffprobePath} -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "${videoUrl}"`;
+  const commandDimensions = `${ffprobePath} -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "${videoUrl}"`;
+  const commandDuration = `${ffprobePath} -v error -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "${videoUrl}"`;
 
   try {
-    const { stdout } = await promisifiedExec(command);
+    const { stdout } = await promisifiedExec(commandDimensions);
     const [width, height] = stdout.trim().split('x');
+
+    const execCommand = await promisifiedExec(commandDuration);
+    const duration = parseFloat(execCommand.stdout.trim());
     // console.log({ width: parseInt(width), height: parseInt(height), err: null });
+
     return {
       width: parseInt(width),
       height: parseInt(height),
+      duration: videoDurationHydration(duration),
       err: null,
     };
   } catch (error) {
     return {
       width: null,
       height: null,
+      duration: null,
       err: error,
     };
     // throw new Error('Erro ao obter dimensões do vídeo:', error);
